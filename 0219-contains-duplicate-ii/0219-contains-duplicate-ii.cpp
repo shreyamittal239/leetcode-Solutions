57f1b03a3d21741a93baab8940d4cbaba23d0f93
Shreya Mittal <shreyamittal239@gmail.com>
2026-08-07 18:17:48 +0530

    Time: 100 ms (34.44%), Space: 124.9 MB (20.67%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int>freq;
+        for(int i = 0; i < nums.size(); i++){
+           
+              if(freq.count(nums[i])){
+                if( i - freq[nums[i]] <=k){
+               
+                 return true;
+                }
+            }
+        
+           freq[nums[i]] = i;
+
+        }
+
+        return false ;
+        
+        
+    }
+};
